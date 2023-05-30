1ab3b4db5408e6c70405f0ec4ace39e364b86cbf
RickoBayu1945 <rickobayu18@gmail.com>
2023-05-30 10:55:59 +0700

    menambahkan file index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Git Test Remot 2</title>
+    <title>Git Remote 2</title>
 </head>
 <body>
-    <h3>Test remote 2</h3>
+    <h2>Lathian Git remote 2</h2>
 </body>
 </html>
